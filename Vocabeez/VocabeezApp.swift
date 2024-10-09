13b908ac2ceea614abdc49e348c73c2c11e4e830
damdaris <darisadam@upi.edu>
2024-10-09 11:45:19 +0700

    Initial Commit

---
+++ Vocabeez/VocabeezApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabeezApp.swift
+//  Vocabeez
+//
+//  Created by Adam Daris Ryadhi on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VocabeezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
